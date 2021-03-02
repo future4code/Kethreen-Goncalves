02d19abb1308a9a86b5cfe55c808dc55045f1e04
Kethreen Lanes <77758983+laneskell@users.noreply.github.com>
2021-03-02 19:51:39 -0300

    EXERCÍCIO AULA15 - PLANNER - FUNCIONANDO COM DESAFIOS

--- semana4/planner/planner-aula15/index.js
+++ semana4/planner/planner-aula15/index.js
@@ -2,38 +2,42 @@
 function salvarTarefa(){
     const tarefaUsuario = document.getElementById("tarefa")
     const diaDaSemana =  document.getElementById("dias-semana")
-    const domingo = document.getElementById("domingo")
-    const segunda = document.getElementById("segunda")
-    const terca = document.getElementById("terca")
-    const quarta = document.getElementById("quarta")
-    const quinta = document.getElementById("quinta")
-    const sexta = document.getElementById("sexta")
-    const sabado = document.getElementById("sabado")
-
-        switch (diaDaSemana.value){
-            case 'domingo':
-               domingo.innerHTML += `<li>${tarefaUsuario.value}</li>`
-                break
-            case 'segunda':
-                segunda.innerHTML += `<li>${tarefaUsuario.value}</li>`
-                break  
-            case 'terca':
-                terca.innerHTML += `<li>${tarefaUsuario.value}</li>`
-                break
-             case 'quarta':
-                quarta.innerHTML += `<li>${tarefaUsuario.value}</li>`
-                break
-            case 'quinta':
-                quinta.innerHTML += `<li>${tarefaUsuario.value}</li>`
-                break  
-            case 'sexta':
-                sexta.innerHTML += `<li>${tarefaUsuario.value}</li>`
-                break
-             case 'sabado':
-                sabado.innerHTML += `<li>${tarefaUsuario.value}</li>`
-                break           
-            default:
-                break
-        }
+ 
+    if (tarefaUsuario.value !== "") {
+        
+               document.getElementById(diaDaSemana.value).innerHTML += `<p  onclick="riscarTarefa(this)" class="itens"> ${tarefaUsuario.value}</p>`
+       
+    }  else{
+        alert('Digite um valor válido!')
+    }
          tarefaUsuario.value=""
 }
+
+function riscarTarefa(element){
+    element.style = "text-decoration: line-through"
+}
+    
+   function limpar(){
+    const limpardomingo = document.querySelector('#domingo')
+    limpardomingo.innerHTML = `<p></p>`
+
+    const limparsegunda = document.querySelector('#segunda')
+    limparsegunda.innerHTML = `<p></p>`
+    
+    const limparterca = document.querySelector('#terca')
+    limparterca.innerHTML = `<p></p>`
+
+    const limparquarta = document.querySelector('#quarta')
+    limparquarta.innerHTML = `<p></p>`
+
+    const limparquinta = document.querySelector('#quinta')
+    limparquinta.innerHTML = `<p></p>`
+
+    const limparsexta = document.querySelector('#sexta')
+    limparsexta.innerHTML = `<p></p>`
+
+    const limparsabado= document.querySelector('#sabado')
+    limparsabado.innerHTML = `<p></p>`
+}
+
+
