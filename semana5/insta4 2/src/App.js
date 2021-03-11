22a161bb5aaf9d7aa5fb11aa625efa15eea7bc17
Kethreen Lanes <77758983+laneskell@users.noreply.github.com>
2021-03-11 08:53:54 -0300

    insta4 react introdução a estados

--- semana5/insta4 2/src/App.js
+++ semana5/insta4 2/src/App.js
@@ -7,11 +7,22 @@ class App extends React.Component {
     return (
       <div className={'app-container'}>
         <Post
-          nomeUsuario={'paulinha'}
+          nomeUsuario={'Kell Lanes'}
+          fotoUsuario={'https://picsum.photos/50/50/?blur=2'}
+          fotoPost={'https://picsum.photos/200/150/?blur=1'}
+        />
+        <Post
+          nomeUsuario={'Clara Macedo'}
           fotoUsuario={'https://picsum.photos/50/50'}
+          fotoPost={'https://picsum.photos/id/237/200/150'}
+        />
+        <Post
+          nomeUsuario={'Isis Callis'}
+          fotoUsuario={'https://picsum.photos/50/50/?blur=3'}
           fotoPost={'https://picsum.photos/200/150'}
         />
       </div>
+      
     );
   }
 }
