1a2ed0400fd824f2aa5b45a56e72177e7455f7ac
laneskell <laneskell@gmail.com>
2021-06-13 19:16:30 -0300

    req createRecipe, req getrecipeById

---
+++ semana18/projeto-cookenu/src/endpoints/recipes/getRecipeById.ts
@@ -0,0 +1,25 @@
+import { recipesTableName } from './../../types';
+import { Request, Response } from "express";
+import connection from "../../connection";
+
+
+export default async function getRecipeById(
+    req: Request,
+    res: Response
+    ): Promise<void> {
+    try{
+        const [recipe] = await connection(recipesTableName)
+            .where({id: req.params.id})
+    
+        res.send({recipe})
+        } catch (error){
+        console.log(error.message);
+        
+        if(res.statusCode === 200){
+            res.status(500).send("Internal server error")
+        } else{
+            res.send(error.message)
+        }
+    }
+    
+}
--- semana18/projeto-cookenu/src/index.ts
+++ semana18/projeto-cookenu/src/index.ts
@@ -3,6 +3,8 @@ import getProfile from "./endpoints/users/getProfile"
 import login from "./endpoints/users/login"
 import signup from "./endpoints/users/signup"
 import getUserById from "./endpoints/users/getUserById"
+import createRecipe from "./endpoints/recipes/createRecipe"
+import getRecipeById from "./endpoints/recipes/getRecipeById"
 
 
 app.post("/users/signup", signup)
@@ -10,6 +12,6 @@ app.post("/users/login", login)
 app.get("/users/profile", getProfile)
 app.get("/users/:id/profile", getUserById)
 
-app.post("/recipe")
-app.get("/recipe/:id")
+app.post("/recipe", createRecipe)
+app.get("/recipe/:id", getRecipeById)
 
